eff73dff521d1b6c8bc1ee6dc954c7d53ff929fd
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2019-11-11 17:47:32 +0530

    Delete option for boxes.

--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Box = props => (
-  <div
-    style={{
-      height: `${props.height}px`,
-      width: `${props.width}px`,
-      backgroundColor: props.color
-    }}
-  ></div>
+  <div>
+    <div
+      style={{
+        height: `${props.height}px`,
+        width: `${props.width}px`,
+        backgroundColor: props.color
+      }}
+    ></div>
+    <button onClick={() => props.removeBox(props.id)}>x</button>
+  </div>
 );
 
 export default Box;
--- src/BoxList.js
+++ src/BoxList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import uuid from 'uuid/v4';
 import Box from "./Box";
 import NewBoxForm from "./NewBoxForm";
 
@@ -10,17 +11,27 @@ class BoxList extends Component {
     };
     this.renderBoxes = this.renderBoxes.bind(this);
     this.addBox = this.addBox.bind(this);
+    this.removeBox = this.removeBox.bind(this);
   }
 
   renderBoxes() {
-    return this.state.boxes.map(box => <Box {...box} />);
+    return this.state.boxes.map(box => <Box {...box} removeBox={this.removeBox} key={box.id}/>);
   }
 
   addBox(box) {
+    let newBox = {...box, id: uuid()};
     this.setState(state => {
       return {
-        boxes: [...state.boxes, box]
+        boxes: [...state.boxes, newBox]
       };
+    });
+  }
+
+  removeBox(id){
+    this.setState(state=>{
+      return {
+        boxes: state.boxes.filter(box=> box.id !== id)
+      }
     });
   }
 
